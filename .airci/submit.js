a1303aa920a72d18462cd9c3fe4b5fe8b3d7efc1
LeeSeungJae <coderoom2018@gmail.com>
2019-10-17 18:51:49 +0900

    edit airci

--- .airci/submit.js
+++ .airci/submit.js
@@ -6,8 +6,6 @@ let studentInfo = require("../student.json");
 let testResult = require("../results.json")
 let { th, name, sprint } = studentInfo;
 let { passed, failed } = testResult;
-// let passed = numPassedTests;
-// let failed = numFailedTests;
 
 console.log(['테스트 결과입니다.', `통과된 테스트: ${passed}`.green, `통과하지 못한 테스트: ${failed}`.red].join('\n'))
 
