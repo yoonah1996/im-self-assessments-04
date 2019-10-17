4971d6880a8b1efc86b2caf26bdb0787cb522cc6
LeeSeungJae <coderoom2018@gmail.com>
2019-10-17 18:08:53 +0900

    add airci

---
+++ __test__/test.js
@@ -0,0 +1,5 @@
+describe('self-assessments-04', function () {
+  require('../async-word-count/async-word-count.test');
+  require('../node-req-count/server.test');
+  require('../sql-publisher-author-genre/sql-publisher-author-genre.test');
+})
